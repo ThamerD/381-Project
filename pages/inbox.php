7321128e08a07c79e21b0d61d91f76c33231c987
Abdullah <aalzughibi2@gmail.com>
2020-03-28 00:47:27 +0300

    new file php
    inbox for chat with name and picture , last message

---
+++ pages/inbox.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style_e.css">
+</head>
+<body>
+<?php include_once "base.php"; ?>
+
+    <div class = "coBody opactiy-Cobody" id="coBody">
+        <h2>Inbox</h2>
+        <div class="Container-inbox">
+            <div class="card-inbox">
+            <img  src="https://profiles.utdallas.edu/img/default.png" width="50px" height = "50px" class = "profile-img padding-inbox" alt="">
+                <p  class="profile-img padding-inbox" ><a href="liveChat.php">Abdullah Ali</a></p>
+                <p class="message-smallSize">Hello i have question</p>
+        </div>
+        
+        <div class="card-inbox">
+            <img  src="https://profiles.utdallas.edu/img/default.png" width="50px" height = "50px" class = "profile-img padding-inbox" alt="">
+                <p  class="profile-img padding-inbox" ><a href="liveChat.php">Thamer Aldaoowd</a></p>
+                <p class="message-smallSize">Can you discount to me ?</p>
+        </div>
+
+        <div class="card-inbox">
+            <img  src="https://profiles.utdallas.edu/img/default.png" width="50px" height = "50px" class = "profile-img padding-inbox" alt="">
+                <p  class="profile-img padding-inbox" ><a href="liveChat.php">Albraa Mohammed</a></p>
+                <p class="message-smallSize">Can you Ship to me?</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
