b1ea35650a411b4cbf84c8625adabc030343b54c
albrae <62217625+albrae@users.noreply.github.com>
2020-04-04 20:47:26 +0300

    cha

--- pages/sign_in.php
+++ pages/sign_in.php
@@ -1,5 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php   
+$nameErr="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if(!$_POST["userNameLogIn"])
+$nameErr.=" &#10006; Enter your name:<br>";
+
+
+if(!$_POST["passLogIn"]){
+$nameErr.=" &#10006; Pleas enetr your password:<br>";
+
+}
+// if($_POST["passLogIn"]){
+// $pass=$_POST["passLogIn"];
+// if(strlen($pass)<8)
+// $nameErr.="password shoud be greter than 8<br>";
+// }
+if($nameErr!=""){
+    $nameErr="<div  style='padding:1%; color: rgb(128, 3, 3);border: 1px solid red; border-color:  rgba(243, 95, 95, 0.322);width:80%;margin-left:10%; border-radius: 3px;background-color: rgba(243, 95, 95, 0.322);  text-align: left;' role='alert'> ".$nameErr."</div>";
+    }
+}
+?>
+     
 
 <head>
     <meta charset="UTF-8">
@@ -10,57 +33,33 @@
 
 <body>
     <?php include_once "base.php"; ?>
-    <div class="divlogin">
-        <div style="margin-top:15%;">
+    <div class="divlogin" style=" height: auto;">
+        <div style="margin-top:5%; padding-bottom:2%;">
             <h2>LOGIN </h2>
+     
 
-            <form>
-                <input class="text-F" type="email" id="tex" name="userNameLogIn" placeholder="&#9993; Email" required><br>
-                <input  class="text-F" type="password" name="passLogIn" placeholder="&#128477;password" required><br>
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" >
+            <div id="error" style="margin-top:0%;margin-bottom:10%;"> <?php  echo $nameErr; ?> </div>
+                <input class="text-F" type="email" id="tex" name="userNameLogIn" placeholder="&#9993; Email" style="width:42%;" required><br>
+                <input  class="text-F" type="password" name="passLogIn" placeholder="&#128477;password" style="width:42%;" required><br>
                 <a href="#" class="linklog" style="   font-size: 12px;margin-right: 22%;">forget password?</a><br>
-                <input type="submit" class="btn" value="Log In"  onclick="fan()">
+                <input type="submit" class="btn" value="Log In"  >
             </form>
             <p>don't have an account?<a href="sign up.html" class="linklog" >sign Up</a></p>
         </div>
     </div>
 
-    
-
-    <script>
-    fan(){
-
-        if(document.getElementbyId('tex').value ==""){
-        alert("mfmfmfm");
-        return false;
-        }
-
-
-
-    }
+ 
     
     
     
-    
-    </script>
+
 
 
 
 
     <i class="fas fa-eye"></i>
-    <script>
-        var x = document.getElementById("nav");
-
-
-        function closeNav() {
-            x.style.width = "0px";
-            x.style.display = "none"
-        }
-
-        function OpenNav() {
-            x.style.width = "200px";
-            x.style.display = "inline"
-        }
-    </script>
+    
 </body>
 
 </html>
--- pages/sign_up.php
+++ pages/sign_up.php
@@ -1,26 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php  
+   
+$nameErr="";
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+// if(!$_POST["fName"]){
+
+
+// }
+if(isset($_POST["fName"]))
+if(!preg_match("/^[A-Za-z]{2,31}$/",$_POST["fName"])){
+    $nameErr.=" &#10006; Enter your name:<br>";
+
+
+    
+}
+
+ if(!preg_match("/^\([0-9]{3}\)-[0-9]{3}-[0-9]{4}$/",$_POST["phone"])){
+   $nameErr.="pleas enter valid phone number<br>"; 
+ }
+
+
+
+
+// $nameErr.=" &#10006; Pleas enetr your password:<br>";
+// }
+// if($_POST["passLogIn"]){
+// $pass=$_POST["passLogIn"];
+// if(strlen($pass)<8)
+// $nameErr.="password shoud be greter than 8<br>";
+// }
+if($nameErr!=""){
+    $nameErr="<div  style='padding:1%; color: rgb(128, 3, 3);border: 1px solid red; border-color:  rgba(243, 95, 95, 0.322);width:80%;margin-left:0%; border-radius: 3px;background-color: rgba(243, 95, 95, 0.322);  text-align: left;' role='alert'> ".$nameErr."</div>";
+    }
+}
+?>
+
+
+
+
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sign in </title>
+    <title>sign up</title>
     <link rel="stylesheet" href="../css/style_e.css">
 </head>
 
 <body>
 
     <?php include_once "base.php"; ?>
-    <div class="divSignUp" id="divup" style="padding-left: 1%;padding-right: 0%;">
+    <div class="divSignUp" id="divup" style="padding-left: 1%;padding-right: 0%; height: auto;">
 
         <h2>Sign Up</h2>
-        <form action="" >
-            full name<span id="sun" style="color: chocolate;">*</span> <br><input type="text" id="fName" placeholder="first name" class="text-F" required>
-            <input type="text" id="lname" placeholder="last name" class="text-F" required> <br> phone number<span style="color: chocolate;">*</span> <input type="text" value="+966" class="text-F" id="number" style="width: 80%;" required><br> Email address<span style="color: chocolate;">*</span>            <input type="email" id="email" placeholder="your@gmail.com" class="text-F" style="width: 80%;" required><br> password
-            <span style="color: chocolate;">*</span> <input type="password" id="pass" placeholder="password" class="text-F" style="width: 80%;" required><br> confirm password<span style="color: chocolate;">*</span><input type="password" id="cpass" placeholder="re-entir password"
-                class="text-F" style="width: 80%;" required>
+        <div  style=""> <?php  echo $nameErr; ?> </div>
+        <!-- <div id="signat" style="background-color:black;padding-top:0%;" ></div> -->
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"  >
+            first name<span id="sun" style="color: chocolate;">*</span> <br><input type="text" id="fName" placeholder="first name" name="fName" class="text-F" style="width:80%;"><br>
+           last name<span id="sun" style="color: chocolate;">*</span> <br><input style="width:80%;" type="text" id="lname" placeholder="last name" class="text-F" > <br> phone number<span style="color: chocolate;">*</span> <input size="10" name="phone" maxlength="12" type="text" id="phon"  value="+966" class="text-F" id="number" style="width: 80%;" ><br> Email address<span style="color: chocolate;">*</span>            <input type="email" id="email" placeholder="your@gmail.com" class="text-F" style="width: 80%;" ><br> password
+            <span style="color: chocolate;">*</span> <input type="password" id="pass" placeholder="password" class="text-F" style="width: 80%;" ><br> confirm password<span style="color: chocolate;">*</span><input type="password" id="cpass" placeholder="re-entir password"
+                class="text-F" style="width: 80%;" >
             <div style="text-align: center;">
-                <input type="submit" value="sign up" class="btn">
+                <input type="submit" value="sign up" class="btn"  >
                 <p>have you account?<a href="sign in.html" class="linklog">Log In</a></p>
         </form>
         </div>
@@ -29,6 +72,53 @@
   
 
     <i class="fas fa-eye"></i>
+
+<!-- 
+<script>
+
+function fun(){
+if(document.getElementById("fName").value.trim()==""){
+    var error="";
+    error+="pleas enter first name";
+    document.getElementById("signat").innerHTML="<div  style='padding:1%; color: rgb(128, 3, 3);border: 1px solid red; border-color:  rgba(243, 95, 95, 0.322);width:80%;margin-left:0%; border-radius: 3px;background-color: rgba(243, 95, 95, 0.322);  text-align: left;'>#"+error+"</div>";
+
+
+
+
+   return false;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+var num=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+if(document.getElementById("phon").value.match(num)){
+    return ture;
+}
+else{
+    var error="";
+    error+="pleas enter falid password";
+    document.getElementById("signat").innerHTML="<div  style='padding:1%; color: rgb(128, 3, 3);border: 1px solid red; border-color:  rgba(243, 95, 95, 0.322);width:80%;margin-left:0%; border-radius: 3px;background-color: rgba(243, 95, 95, 0.322);  text-align: left;'>#"+error+"</div>";
+// return false;
+}
+
+
+
+}
+
+
+</script> -->
+
+
 </body>
 
 </html>
