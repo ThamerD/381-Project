ece10f7182eeca7205706fe079a9f935ce228b37
ThamerD <tha-mer@hotmail.com>
2020-04-19 23:23:17 +0300

    Extra details in product page

--- pages/Product.php
+++ pages/Product.php
@@ -35,11 +35,11 @@
         }
         .container {
         float: none;
-        width: 50%;    
+        width: 100%;    
       }
       .prodInfo{
         float: none;
-        width: 50%;
+        width: 100%;
     }
     }
     * {
@@ -61,6 +61,8 @@ $conn=mysqli_connect("localhost","root","","381_db");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+
 
 // var qs = new Querystring();
 // var pid = qs.get("ID");
@@ -84,6 +86,21 @@ $im2 = mysqli_query($conn,"SELECT IMAGE_DIR FROM `product_images` WHERE ID = '$p
 $imgNum=1;
 $imgNum2=1;
 
+$pCat =  $row['CATEGORY'];
+$pCond =  $row['USED'] ;
+$pAuth =  $row['BOOK_AUTHOR'] ;
+$pISBN =  $row['BOOK_ISBN'];
+$pPages =  $row['BOOK_NUM_PAGE'];
+$pBCond =  $row['BOOK_CONDITION'];
+$pBrand =  $row['BRAND'];
+$pODO =  $row['CAR_ODO'];
+$pModel =  $row['CAR_YEAR'];
+$pSize =  $row['CLOTHING_SIZE'];
+$pGenre =  $row['GENRE'];
+$pPlatform =  $row['GAME_PLATFORM'];
+$pYear =  $row['MOVIE_YEAR'];
+
+
 
 
 echo"
@@ -94,22 +111,58 @@ echo"
     <a class='author' href='../pages/liveChat.php'>0$aNum</a>
       </span>
 </p>
-<br>
+<br>";
 
+
+echo"
 <div class='prodInfo'>
   <h3 style='text-align: center;'>$pName</h3>
     <!-- <h3 style='text-align: center;'>ASUS VivoBook S15 S532 Thin & Light 15.6' FHD, Intel Core i7-8565U CPU, 8 GB DDR4 RAM, PCIe NVMe 512 GB SSD, Windows 10 Home, S532FA-SB77, Transparent Silver</h3> -->
-    <!-- <hr> -->
+    <hr>
     <p style='font-size: 20px; margin: 0%'>Price: <span id='prc' style='color: firebrick;'>$pPrice</span></p>
     <br>
-    <p style='white-space: pre-wrap;'>
-$pDesc
-</p>
-    <!-- <hr> -->
+    <p style='white-space: pre-wrap;'>$pDesc</p>
+    <hr>";
 
-</div>
-
+    if($pCat!=null && $pCat!='0')
+    echo"<p> Category: $pCat</p>";
+    
+    if($pCond!=null && $pCond!='0')
+    echo"<p> Condition: $pCond</p>";
+    
+    if($pAuth!=null && $pAuth!='0')
+    echo"<p> Author: $pAuth</p>";
+    
+    if($pISBN!=null && $pISBN!='0')
+    echo"<p> ISBN: $pISBN</p>";
+    
+    if($pPages!=null && $pPages!='0')
+    echo"<p> Number of pages: $pPages</p>";
+    
+    if($pBCond!=null && $pBCond!='0')
+    echo"<p> Condition: $pBCond</p>";
+    
+    if($pBrand!=null && $pBrand!='0')
+    echo"<p> Brand: $pBrand</p>";
+    
+    if($pODO!=null && $pODO!='0')
+    echo"<p> ODO: $pODO</p>";
+    
+    if($pModel!=null && $pModel!='0')
+    echo"<p> Model: $pModel</p>";
+    
+    if($pGenre!=null && $pGenre!='0')
+    echo"<p> Genre: $pGenre</p>";
+    
+    if($pPlatform!=null && $pPlatform!='0')
+    echo"<p> Platform: $pPlatform</p>";
+    
+    if($pYear!=null && $pYear!='0')
+    echo"<p> Release year: $pYear</p>";
+    
+echo "</div>
 <div class='container'>";
+
 if (mysqli_num_rows($im)>0)
 while( $images = mysqli_fetch_assoc($im) ){
   $photo = $images['IMAGE_DIR'];
